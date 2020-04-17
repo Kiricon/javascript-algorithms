3e48d2919348d349f1825a75732e858f48ad17b2
Dominic Valenciana <dominic.valenciana@alaskaair.com>
2020-04-17 12:06:42 -0700

    wrapped up queue.js

--- src/data-structures/queue/Queue.js
+++ src/data-structures/queue/Queue.js
@@ -2,57 +2,27 @@ import LinkedList from '../linked-list/LinkedList';
 
 export default class Queue {
   constructor() {
-    // We're going to implement Queue based on LinkedList since the two
-    // structures are quite similar. Namely, they both operate mostly on
-    // the elements at the beginning and the end. Compare enqueue/dequeue
-    // operations of Queue with append/deleteHead operations of LinkedList.
     this.linkedList = new LinkedList();
   }
 
-  /**
-   * @return {boolean}
-   */
-  isEmpty() {
-    return !this.linkedList.head;
+  enqueue(val) {
+    return this.linkedList.append(val);
   }
 
-  /**
-   * Read the element at the front of the queue without removing it.
-   * @return {*}
-   */
-  peek() {
-    if (!this.linkedList.head) {
-      return null;
-    }
-
-    return this.linkedList.head.value;
-  }
-
-  /**
-   * Add a new element to the end of the queue (the tail of the linked list).
-   * This element will be processed after all elements ahead of it.
-   * @param {*} value
-   */
-  enqueue(value) {
-    this.linkedList.append(value);
-  }
-
-  /**
-   * Remove the element at the front of the queue (the head of the linked list).
-   * If the queue is empty, return null.
-   * @return {*}
-   */
   dequeue() {
-    const removedHead = this.linkedList.deleteHead();
-    return removedHead ? removedHead.value : null;
+    const deleteHead = this.linkedList.deleteHead();
+    return deleteHead ? deleteHead.value : null;
   }
 
-  /**
-   * @param [callback]
-   * @return {string}
-   */
+  peek() {
+    return this.linkedList.tail ? this.linkedList.head.value : null;
+  }
+
+  isEmpty() {
+    return this.linkedList.head == null;
+  }
+
   toString(callback) {
-    // Return string representation of the queue's linked list.
     return this.linkedList.toString(callback);
   }
 }
