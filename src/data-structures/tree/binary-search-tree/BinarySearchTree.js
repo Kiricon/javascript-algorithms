1f54d49e7a3f3e4c15471beff2cd801c107f98b3
Dominic Valenciana <dominic.valenciana@alaskaair.com>
2020-04-23 08:38:30 -0700

    Finished binary search tree

--- src/data-structures/tree/binary-search-tree/BinarySearchTree.js
+++ src/data-structures/tree/binary-search-tree/BinarySearchTree.js
@@ -1,44 +1,15 @@
 import BinarySearchTreeNode from './BinarySearchTreeNode';
 
 export default class BinarySearchTree {
-  /**
-   * @param {function} [nodeValueCompareFunction]
-   */
-  constructor(nodeValueCompareFunction) {
-    this.root = new BinarySearchTreeNode(null, nodeValueCompareFunction);
-
-    // Steal node comparator from the root.
-    this.nodeComparator = this.root.nodeComparator;
+  constructor() {
+    this.root = new BinarySearchTreeNode();
   }
 
-  /**
-   * @param {*} value
-   * @return {BinarySearchTreeNode}
-   */
-  insert(value) {
-    return this.root.insert(value);
-  }
+  insert(val) { return this.root.insert(val); }
 
-  /**
-   * @param {*} value
-   * @return {boolean}
-   */
-  contains(value) {
-    return this.root.contains(value);
-  }
+  toString() { return this.root.toString(); }
 
-  /**
-   * @param {*} value
-   * @return {boolean}
-   */
-  remove(value) {
-    return this.root.remove(value);
-  }
+  contains(val) { return this.root.contains(val); }
 
-  /**
-   * @return {string}
-   */
-  toString() {
-    return this.root.toString();
-  }
+  remove(val) { return this.root.remove(val); }
 }
