49484c4dfb0967c1b084262c01ababf3539af606
Dominic Valenciana <dominic.valenciana@alaskaair.com>
2020-08-07 12:03:57 -0700

    finished powersets

--- src/algorithms/sets/power-set/btPowerSet.js
+++ src/algorithms/sets/power-set/btPowerSet.js
@@ -1,41 +1,17 @@
-/**
- * @param {*[]} originalSet - Original set of elements we're forming power-set of.
- * @param {*[][]} allSubsets - All subsets that have been formed so far.
- * @param {*[]} currentSubSet - Current subset that we're forming at the moment.
- * @param {number} startAt - The position of in original set we're starting to form current subset.
- * @return {*[][]} - All subsets of original set.
- */
-function btPowerSetRecursive(originalSet, allSubsets = [[]], currentSubSet = [], startAt = 0) {
-  // Let's iterate over originalSet elements that may be added to the subset
-  // without having duplicates. The value of startAt prevents adding the duplicates.
-  for (let position = startAt; position < originalSet.length; position += 1) {
-    // Let's push current element to the subset
-    currentSubSet.push(originalSet[position]);
+function findPowerSet(originalSet, currSet, sets, position) {
+  if (currSet.length > originalSet.length) return;
 
-    // Current subset is already valid so let's memorize it.
-    // We do array destruction here to save the clone of the currentSubSet.
-    // We need to save a clone since the original currentSubSet is going to be
-    // mutated in further recursive calls.
-    allSubsets.push([...currentSubSet]);
+  sets.push([...currSet]);
 
-    // Let's try to generate all other subsets for the current subset.
-    // We're increasing the position by one to avoid duplicates in subset.
-    btPowerSetRecursive(originalSet, allSubsets, currentSubSet, position + 1);
-
-    // BACKTRACK. Exclude last element from the subset and try the next valid one.
-    currentSubSet.pop();
+  for (let i = position; i < originalSet.length; i++) {
+    currSet.push(originalSet[i]);
+    findPowerSet(originalSet, currSet, sets, i + 1);
+    currSet.pop();
   }
-
-  // Return all subsets of a set.
-  return allSubsets;
 }
 
-/**
- * Find power-set of a set using BACKTRACKING approach.
- *
- * @param {*[]} originalSet
- * @return {*[][]}
- */
 export default function btPowerSet(originalSet) {
-  return btPowerSetRecursive(originalSet);
+  const sets = [];
+  findPowerSet(originalSet, [], sets, 0);
+  return sets;
 }
