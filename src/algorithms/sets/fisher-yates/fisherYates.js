843c1644fb8728a96ebe791c3d101f5004e02753
Dominic Valenciana <dominic.valenciana@alaskaair.com>
2020-08-07 08:30:51 -0700

    fisher-yates shuffle done

--- src/algorithms/sets/fisher-yates/fisherYates.js
+++ src/algorithms/sets/fisher-yates/fisherYates.js
@@ -1,15 +1,18 @@
-/**
- * @param {*[]} originalArray
- * @return {*[]}
- */
-export default function fisherYates(originalArray) {
-  // Clone array from preventing original array from modification (for testing purpose).
-  const array = originalArray.slice(0);
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
-  for (let i = (array.length - 1); i > 0; i -= 1) {
-    const randomIndex = Math.floor(Math.random() * (i + 1));
-    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+export default function fisherYates(arr) {
+  const len = arr.length;
+  const newArr = [...arr];
+
+  if (len < 2 || !arr) return arr;
+  for (let i = len - 1; i >= 0; i--) {
+    const randomIndex = getRandomInt(i);
+    const temp = newArr[randomIndex];
+    newArr[randomIndex] = newArr[i];
+    newArr[i] = temp;
   }
 
-  return array;
+  return newArr;
 }
